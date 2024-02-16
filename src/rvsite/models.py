a261c41b717d4431487535e7af670c1ca9a1cffa
Gareth Simpson <g@xurble.org>
2024-02-16 23:00:12 +0000

    Lint

--- src/rvsite/models.py
+++ src/rvsite/models.py
@@ -5,41 +5,41 @@ from django.contrib.auth.models import User
 # Create your models here.
 import datetime
 
+
 class RVDomain(models.Model):
-    name       = models.CharField(max_length=32,unique=True)
-    alt_domain = models.CharField(max_length=128,blank=True,default='',db_index=True)
+    name = models.CharField(max_length=32, unique=True)
+    alt_domain = models.CharField(max_length=128, blank=True, default='', db_index=True)
 
-    min_year      = models.IntegerField(default=0)
-    max_year      = models.IntegerField(default=0)
+    min_year = models.IntegerField(default=0)
+    max_year = models.IntegerField(default=0)
 
-    owner         = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    display_name  = models.CharField(max_length=128,default='RearVue')
-    poster_image  = models.ForeignKey('RVItem',null=True,blank=True, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=128, default='RearVue')
+    poster_image = models.ForeignKey('RVItem', null=True, blank=True, on_delete=models.CASCADE)
 
-    blurb         = models.TextField(null=True,blank=True,default='')
+    blurb = models.TextField(null=True, blank=True, default='')
 
-    last_updated  = models.DateTimeField(null=True, blank=True)
+    last_updated = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
-        return "%s - %s" % (self.name,self.display_name)
+        return "%s - %s" % (self.name, self.display_name)
 
 
 class RVService(models.Model):
-    name             = models.CharField(max_length=512)
-    domain           = models.ForeignKey(RVDomain, on_delete=models.CASCADE)
-    type             = models.CharField(max_length=128)
-    last_checked     = models.DateTimeField(default=datetime.datetime(2015, 1, 10, 17, 26, 51, 977260)) #old date makes it get checked right away
-    username         = models.CharField(max_length=128,blank=True,default='')
-    userid           = models.CharField(max_length=128,blank=True,default='')
-    profile_pic      = models.CharField(max_length=512,blank=True,default='')
-    max_update_id    = models.CharField(max_length=256,blank=True,default='')
-    auth_token       = models.CharField(max_length=256,blank=True,default='')
-    auth_secret      = models.CharField(max_length=256,blank=True,default='')
-    live             = models.BooleanField(default=True)
+
+    name = models.CharField(max_length=512)
+    domain = models.ForeignKey(RVDomain, on_delete=models.CASCADE)
+    type = models.CharField(max_length=128)
+    last_checked = models.DateTimeField(default=datetime.datetime(2015, 1, 10, 17, 26, 51, 977260))  # old date makes it get checked right away
+    username = models.CharField(max_length=128, blank=True, default='')
+    userid = models.CharField(max_length=128, blank=True, default='')
+    profile_pic = models.CharField(max_length=512, blank=True, default='')
+    max_update_id = models.CharField(max_length=256, blank=True, default='')
+    auth_token = models.CharField(max_length=256, blank=True, default='')
+    auth_secret = models.CharField(max_length=256, blank=True, default='')
+    live = models.BooleanField(default=True)
     hide_unmoderated = models.BooleanField(default=False)
-
-
 
     def __str__(self):
 
@@ -48,28 +48,28 @@ class RVService(models.Model):
 
 class RVItem(models.Model):
 
-    service          = models.ForeignKey(RVService, on_delete=models.CASCADE)
-    domain           = models.ForeignKey(RVDomain, on_delete=models.CASCADE)
+    service = models.ForeignKey(RVService, on_delete=models.CASCADE)
+    domain = models.ForeignKey(RVDomain, on_delete=models.CASCADE)
 
-    item_id          = models.CharField(max_length=128,db_index=True)
+    item_id = models.CharField(max_length=128, db_index=True)
 
-    date_retrieved   = models.DateTimeField(auto_now_add=True)
-    date_created     = models.DateField(db_index=True)
+    date_retrieved = models.DateTimeField(auto_now_add=True)
+    date_created = models.DateField(db_index=True)
     datetime_created = models.DateTimeField(db_index=True)
 
-    remote_url       = models.CharField(max_length=512,blank=True,default='')
+    remote_url = models.CharField(max_length=512, blank=True, default='')
 
-    title            = models.CharField(max_length=512,blank=True,default='')
-    caption          = models.TextField(blank=True,default='')
+    title = models.CharField(max_length=512, blank=True, default='')
+    caption = models.TextField(blank=True, default='')
 
-    public           = models.BooleanField(default=True)
+    public = models.BooleanField(default=True)
 
-    raw_data         = models.TextField(blank=True,default='')
+    raw_data = models.TextField(blank=True, default='')
 
-    mirror_state     = models.IntegerField(default=0)
+    mirror_state = models.IntegerField(default=0)
 
-    moderated        = models.BooleanField(default=False)
-    edited           = models.BooleanField(default=False)
+    moderated = models.BooleanField(default=False)
+    edited = models.BooleanField(default=False)
 
     def __str__(self):
         return "%s - %s (%d)" % (self.remote_url, self.service.name, self.mirror_state)
@@ -103,7 +103,7 @@ class RVItem(models.Model):
     def thumbnail(self):
         try:
             return self.rvmedia_set.first().thumbnail
-        except:
+        except Exception:
             return ""
 
     @property
@@ -118,7 +118,6 @@ class RVItem(models.Model):
     def original_media(self):
         return self.rvmedia_set.first().original_media
 
-
     @property
     def orginal_links(self):
         return self.rvlink_set.filter(is_context=False)
@@ -126,8 +125,6 @@ class RVItem(models.Model):
     @property
     def context_links(self):
         return self.rvlink_set.filter(is_context=True)
-
-
 
     @property
     def media_list(self):
@@ -139,56 +136,84 @@ class RVItem(models.Model):
         return items
 
 
-
 class RVLink(models.Model):
 
-    item        = models.ForeignKey(RVItem, on_delete=models.CASCADE)
+    item = models.ForeignKey(RVItem, on_delete=models.CASCADE)
 
-    url         = models.CharField(max_length=512)
-    title       = models.CharField(max_length=512,blank=True,default='')
+    url = models.CharField(max_length=512)
+    title = models.CharField(max_length=512, blank=True, default='')
     description = models.TextField(blank=True, default='')
-    image       = models.CharField(max_length=512,blank=True,default='')
-    is_context  = models.BooleanField(default=False)
+    image = models.CharField(max_length=512, blank=True, default='')
+    is_context = models.BooleanField(default=False)
 
-    def make_image_path(self,file_type):
+    def make_image_path(self, file_type):
 
         if "?" in file_type:
             file_type = file_type.split("?")[0]
 
-        self.original_media = "media/%s/%d/%02d/%02d/%s_%d_link.%s" % (self.item.domain.name,self.item.date_created.year,self.item.date_created.month,self.item.date_created.day,self.item.service.type,self.id,file_type)
+        self.original_media = "media/%s/%d/%02d/%02d/%s_%d_link.%s" % (
+                                    self.item.domain.name,
+                                    self.item.date_created.year,
+                                    self.item.date_created.month,
+                                    self.item.date_created.day,
+                                    self.item.service.type,
+                                    self.id,
+                                    file_type
+                                )
         return self.image
-
 
 
 class RVMedia(models.Model):
 
     item = models.ForeignKey(RVItem, on_delete=models.CASCADE)
 
-    original_media   = models.CharField(max_length=256,blank=True,default='')
+    original_media = models.CharField(max_length=256, blank=True, default='')
 
-    primary_media    = models.CharField(max_length=256,blank=True,default='')
-    media_type       = models.IntegerField(default=0,choices=((0,"None"), (1,"Image"), (2,"Video"), (3, "Autoplaying Video")))
-    thumbnail        = models.CharField(max_length=256,blank=True,default='')
+    primary_media = models.CharField(max_length=256, blank=True, default='')
+    media_type = models.IntegerField(default=0, choices=((0, "None"), (1, "Image"), (2, "Video"), (3, "Autoplaying Video")))
+    thumbnail = models.CharField(max_length=256, blank=True, default='')
 
-
-    def make_original_path(self,file_type):
+    def make_original_path(self, file_type):
         if "?" in file_type:
             file_type = file_type.split("?")[0]
 
-        self.original_media = "media/%s/%d/%02d/%02d/%s_%d_o.%s" % (self.item.domain.name,self.item.date_created.year,self.item.date_created.month,self.item.date_created.day,self.item.service.type,self.id,file_type)
+        self.original_media = "media/%s/%d/%02d/%02d/%s_%d_o.%s" % (
+                                            self.item.domain.name,
+                                            self.item.date_created.year,
+                                            self.item.date_created.month,
+                                            self.item.date_created.day,
+                                            self.item.service.type,
+                                            self.id,
+                                            file_type
+                                        )
         return self.original_media
 
-    def make_primary_path(self,file_type):
+    def make_primary_path(self, file_type):
         if "?" in file_type:
             file_type = file_type.split("?")[0]
 
-        self.primary_media = "media/%s/%d/%02d/%02d/%s_%d_p.%s" % (self.item.domain.name,self.item.date_created.year,self.item.date_created.month,self.item.date_created.day,self.item.service.type,self.id,file_type)
+        self.primary_media = "media/%s/%d/%02d/%02d/%s_%d_p.%s" % (
+                                            self.item.domain.name,
+                                            self.item.date_created.year,
+                                            self.item.date_created.month,
+                                            self.item.date_created.day,
+                                            self.item.service.type,
+                                            self.id,
+                                            file_type
+                                        )
         return self.primary_media
 
-    def make_thumbnail_path(self,file_type):
+    def make_thumbnail_path(self, file_type):
         if "?" in file_type:
             file_type = file_type.split("?")[0]
 
-        self.thumbnail =  "media/%s/%d/%02d/%02d/%s_%d_t.%s" % (self.item.domain.name,self.item.date_created.year,self.item.date_created.month,self.item.date_created.day,self.item.service.type,self.id,file_type)
+        self.thumbnail = "media/%s/%d/%02d/%02d/%s_%d_t.%s" % (
+                                            self.item.domain.name,
+                                            self.item.date_created.year,
+                                            self.item.date_created.month,
+                                            self.item.date_created.day,
+                                            self.item.service.type,
+                                            self.id,
+                                            file_type
+                                        )
         return self.thumbnail
-
--- src/rvsite/urls.py
+++ src/rvsite/urls.py
@@ -1,17 +1,21 @@
 from django.urls import path
 
-
-
-from .views import *
+from .views import (
+    index,
+    show_item,
+    show_day,
+    show_month,
+    show_year,
+)
 
 urlpatterns = [
     # Examples:
     path('', index, name='index'),
 
-    path('rv/<int:year>/<int:month>/<int:day>/<int:iid>/',show_item, name='show_item'),
-    path('rv/<int:year>/<int:month>/<int:day>/',show_day, name='show_day'),
-    path('rv/<int:year>/<int:month>/',show_month, name='show_month'),
-    path('rv/<int:year>/',show_year, name='show_year'),
+    path('rv/<int:year>/<int:month>/<int:day>/<int:iid>/', show_item, name='show_item'),
+    path('rv/<int:year>/<int:month>/<int:day>/', show_day, name='show_day'),
+    path('rv/<int:year>/<int:month>/', show_month, name='show_month'),
+    path('rv/<int:year>/', show_year, name='show_year'),
 ]
 
 
